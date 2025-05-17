e98b386a22157b2405ad7e65c1a54acb5af6779d
RuslanGon <ruslan441006@gmail.com>
2025-05-17 11:54:37 +0300

    try

---
+++ server/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from 'cors'
+import startServer from "./db.js";
+
+
+const app = express();
+dotenv.config();
+app.use(cors())
+app.use(express.json())
+
+startServer(app);
