03e9c9df77d2b3e28b5e687fd3ff4925e68fbff8
RuslanGon <ruslan441006@gmail.com>
2025-05-17 18:33:24 +0300

    wer

--- client/pages/LoginPage/LoginPage.jsx
+++ client/pages/LoginPage/LoginPage.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import css from './LoginPage.module.css';
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
-  )
-}
+    <form className={css.form}>
+      <h2 className={css.title}>Login</h2>
+      <label className={css.field}>
+        <span className={css.label}>Name</span>
+        <input
+          className={css.input}
+          type="text"
+          name="name"
+          placeholder="Enter your name"
+          required
+        />
+      </label>
+      <label className={css.field}>
+        <span className={css.label}>Password</span>
+        <input
+          className={css.input}
+          type="password"
+          name="password"
+          placeholder="Enter your password"
+          required
+        />
+      </label>
+      <button type="submit" className={css.button}>Login </button>
+      <p className={css.redirect}> Already login?{' '}
+        <Link to="/register" className={css.link}> Sign Up </Link>
+      </p>
+    </form>
+  );
+};
 
-export default LoginPage
+export default LoginPage;
--- client/pages/RegisterPage/RegisterPage.jsx
+++ client/pages/RegisterPage/RegisterPage.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import css from './RegisterPage.module.css';
 
 const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
-  )
-}
+    <form className={css.form}>
+      <h2 className={css.title}>Register</h2>
+      <label className={css.field}>
+        <span className={css.label}>Name</span>
+        <input
+          className={css.input}
+          type="text"
+          name="name"
+          placeholder="Enter your name"
+          required
+        />
+      </label>
+      <label className={css.field}>
+        <span className={css.label}>Email</span>
+        <input
+          className={css.input}
+          type="email"
+          name="email"
+          placeholder="Enter your email"
+          required
+        />
+      </label>
+      <label className={css.field}>
+        <span className={css.label}>Password</span>
+        <input
+          className={css.input}
+          type="password"
+          name="password"
+          placeholder="Enter your password"
+          required
+        />
+      </label>
+      <button type="submit" className={css.button}> Sign Up</button>
+      <p className={css.redirect}> Already registered?{' '}
+        <Link to="/login" className={css.link}> Login here</Link>
+      </p>
+    </form>
+  );
+};
 
-export default RegisterPage
+export default RegisterPage;
