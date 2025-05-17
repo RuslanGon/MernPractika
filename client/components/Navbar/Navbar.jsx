52d5d6e06d584e7a99bac6b01c00f5bc3f4f2971
RuslanGon <ruslan441006@gmail.com>
2025-05-17 19:06:28 +0300

    wer

--- client/components/Navbar/Navbar.jsx
+++ client/components/Navbar/Navbar.jsx
@@ -2,17 +2,19 @@ import React from "react";
 import logo from "../../src/assets/logo.jpg";
 import css from "./Navbar.module.css";
 import { Link } from "react-router-dom";
+import {isAuthenticated} from '../../src/utils/auth.js'
 
 const Navbar = () => {
+    const isLoggedIn = isAuthenticated();
   return (
     <div className={css.container}>
-      <img className={css.logo} src={logo} alt="logo" />
-      <div className={css.div}>
-      <Link className={css.link} to="/camper">Camper</Link>
-        <Link className={css.link} to="/login">Login</Link>
-        <Link className={css.link} to="/register">Register</Link>
-      </div>
+    <img className={css.logo} src={logo} alt="logo" />
+    <div className={css.div}>
+      {isLoggedIn && ( <Link className={css.link} to="/camper">Camper</Link>)}
+      <Link className={css.link} to="/login">Login</Link>
+      <Link className={css.link} to="/register">Register</Link>
     </div>
+  </div>
   );
 };
 
