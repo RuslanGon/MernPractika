cc7d776b804f62b9a4bc7b5b353a8b3b720aa6ea
RuslanGon <ruslan441006@gmail.com>
2025-05-21 16:12:45 +0300

    wer

--- client/pages/MyCamper/MyCamperPage.jsx
+++ client/pages/MyCamper/MyCamperPage.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const MyCamperPage = () => {
-  return (
-    <div>MyCamperPage</div>
-  )
-}
+  const [camper, setCamper] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-export default MyCamperPage
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const { data } = await axios.get("http://localhost:3001/campers/get");
+        console.log(data);
+        setCamper(data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>My camper</h1>
+      <ul>
+        <li>
+          <h2>Name: </h2>
+          <p>Price</p>
+          <p>Rating:</p>
+          <p>Location:</p>
+          <p>Description:</p>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default MyCamperPage;
